7167bd80021ab41b34a88554a88f90f9648d548d
Baptistemontan <baptiste.demontangon@gmail.com>
2022-12-29 10:32:49 +0100

    added comments for the Props trait

--- next_rs_traits/src/props.rs
+++ next_rs_traits/src/props.rs
@@ -2,7 +2,12 @@ use sycamore::prelude::*;
 use serde::{Serialize, de::DeserializeOwned};
 
 
-pub trait Props: Send + 'static + IntoProps + Serialize + DeserializeOwned { }
+/// Need Send for use in async functions, 
+/// need DeserializeOwned cause it will be converted to ReactiveProps anyway,
+/// which can't have value borrowed from the serialized string or the url.
+/// And need 'static for the same reason, there is nothing to borrow from so It makes 
+/// my life easier.
+pub trait Props: Send + IntoProps + Serialize + DeserializeOwned + 'static { }
 
 
 pub trait ReactiveProps<'a> {
