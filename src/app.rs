6a88b3c5da7a2326f23c734f6e13a5b630e01356
Baptistemontan <baptiste.demontangon@gmail.com>
2022-12-29 16:36:07 +0100

    moved the html generation functions

--- src/app.rs
+++ src/app.rs
@@ -5,6 +5,7 @@ use super::pages::DynPages;
 use super::prelude::*;
 use next_rs_traits::layout::DynLayout;
 use next_rs_traits::pages::{DynComponent, DynPageDyn};
+use sycamore::prelude::*;
 
 pub const SERIALIZED_PROPS_KEY: &str = "NEXT_RS_SERIALIZED_PROPS";
 pub const NEXT_RS_WINDOW_OBJECT_KEY: &str = "__NEXT_RS__";
@@ -78,5 +79,34 @@ impl AppInner {
 
     pub fn not_found_page(&self) -> &dyn DynComponent {
         &*self.not_found_page
+    }
+}
+
+fn window_object_script(props: &str) -> String { 
+   format!("window.{0}=window.{0}||{{}};window.{0}.{1}=\'{2}\'", NEXT_RS_WINDOW_OBJECT_KEY, SERIALIZED_PROPS_KEY, props)
+}
+
+fn default_head<G: Html>(cx: Scope, head: View<G>, props: &str) -> View<G> {
+    let script = window_object_script(props);
+    view! { cx,
+        head {
+            meta(charset = "UTF-8")
+            meta(http-equiv="X-UA-Compatible", content="IE=edge")
+            meta(name="viewport", content="width=device-width, initial-scale=1.0")
+            script {
+                (script)
+            }
+            (head)
+        }
+    }
+}
+
+pub fn default_html_view<G: Html>(cx: Scope, body: View<G>, head: View<G>, props: &str) -> View<G> {
+    let head = default_head(cx, head, props);
+    view! { cx,
+        (head)
+        body {
+            (body)
+        }
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use crate::app::{AppInner, NEXT_RS_WINDOW_OBJECT_KEY, SERIALIZED_PROPS_KEY};
+use crate::app::{AppInner, default_html_view};
 
 use super::pages::DynPages;
 use super::prelude::*;
@@ -43,34 +43,15 @@ impl Server {
     pub async fn render_to_string<'url>(&self, url_infos: &UrlInfos<'url>) -> String {
         let (page, props) = self.find_page_and_props(url_infos).await;
         let serialized_props = unsafe { page.serialize_props(&props).unwrap() };
-        let script = Self::window_object_script(&serialized_props);
         let html = sycamore::render_to_string(|cx| {
-            let props = unsafe { page.render_server(cx, props) };
-            let body = self.layout().render_server(cx, props);
-            default_document_view(cx, body, script)
+            let app = unsafe { page.render_server(cx, props) };
+            let body = self.layout().render_server(cx, app);
+            let head = view!{ cx,  };
+            default_html_view(cx, body, head, &serialized_props)
         });
         html
     }
 
-    fn window_object_script(props: &str) -> String {
-        format!("window.{0}=window.{0}||{{}};window.{0}.{1}=\'{2}\'", NEXT_RS_WINDOW_OBJECT_KEY, SERIALIZED_PROPS_KEY, props)
-    }
+    
 }
 
-fn default_document_view<G: Html>(cx: Scope, body: View<G>, script: String) -> View<G> {
-    view! { cx,
-        html(lang = "en") {
-            head {
-                meta(charset = "UTF-8")
-                meta(http-equiv="X-UA-Compatible", content="IE=edge")
-                meta(name="viewport", content="width=device-width, initial-scale=1.0")
-                script {
-                    (script)
-                }
-            }
-            body {
-                (body)
-            }
-        }
-    }
-}
