c9848a6b953615742951fc8d75c18531e2ba350e
Baptistemontan <baptiste.demontangon@gmail.com>
2022-12-30 19:43:03 +0100

    smol comment

--- next_rs_traits/src/api.rs
+++ next_rs_traits/src/api.rs
@@ -1,11 +1,12 @@
-use crate::routes::DynRoutable;
+use super::routes::DynRoutable;
 use super::pointers::*;
-
 use super::predule::*;
 
 /// For now api routes just respond with a string,
 /// this will change just need to figure out the api.
-/// Also need a way for ressources, got a rough idea how to do it but will se later.s
+/// Also need a way for ressources,
+/// they could be store in a HashMap<TypeId, Box<dyn Any>>,
+/// but I will take care of that later.
 #[async_trait::async_trait]
 pub trait Api: Routable {
     async fn respond<'url>(route: Self::Route<'url>) -> String;
