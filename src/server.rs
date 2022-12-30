1d1cecbf90cd057359ae04bd87d8d9733adc77a1
Baptistemontan <baptiste.demontangon@gmail.com>
2022-12-30 17:49:43 +0100

    now return none if route not found instead of defaulting to not found, and added function to render not found

--- src/server.rs
+++ src/server.rs
@@ -34,21 +34,40 @@ impl Server {
         self.inner.layout()
     }
 
+    pub async fn try_find_page_and_props<'url>(&self, url_infos: &UrlInfos<'url>) -> Option<(&'_ dyn DynComponent, PropsUntypedPtr)> {
+        if let Some(page) = self.static_pages().find_static_page(url_infos) {
+            return Some((page.as_dyn_component(), PropsUntypedPtr::new_unit()));
+        }
+        if let Some((page, props)) = self.dyn_pages().find_dyn_page_and_props(url_infos).await {
+            return Some((page.as_dyn_component(), props));
+        }
+        None
+    }
+
     pub async fn find_page_and_props<'url>(
         &self,
         url_infos: &UrlInfos<'url>,
     ) -> (&'_ dyn DynComponent, PropsUntypedPtr) {
-        if let Some(page) = self.static_pages().find_static_page(url_infos) {
-            return (page.as_dyn_component(), PropsUntypedPtr::new_unit());
-        }
-        if let Some((page, props)) = self.dyn_pages().find_dyn_page_and_props(url_infos).await {
-            return (page.as_dyn_component(), props);
-        }
-        (self.not_found_page(), NotFoundPageProps::new_untyped())
+        self.try_find_page_and_props(url_infos).await
+            .unwrap_or_else(|| (self.not_found_page(), NotFoundPageProps::new_untyped()))
     }
 
-    pub async fn render_to_string<'url>(&self, url_infos: &UrlInfos<'url>) -> String {
-        let (page, props) = self.find_page_and_props(url_infos).await;
+    pub async fn try_render_to_string<'url>(&self, url_infos: &UrlInfos<'url>) -> Option<String> {
+        let (page, props) = self.try_find_page_and_props(url_infos).await?;
+        let serialized_props = unsafe { page.serialize_props(&props).unwrap() };
+        let html = sycamore::render_to_string(|cx| {
+            let DynRenderResult { body, head } = unsafe { page.render_server(cx, props) };
+            let body = self.layout().render_server(cx, body);
+            default_html_view(cx, body, head, &serialized_props)
+        });
+        Some(format!(
+            "<!DOCTYPE html><html id=\"{}\">{}</html>",
+            ROOT_ELEMENT_ID, html
+        ))
+    }
+
+    pub fn render_not_found(&self) -> String {
+        let (page, props) = (self.not_found_page(), NotFoundPageProps::new_untyped());
         let serialized_props = unsafe { page.serialize_props(&props).unwrap() };
         let html = sycamore::render_to_string(|cx| {
             let DynRenderResult { body, head } = unsafe { page.render_server(cx, props) };
