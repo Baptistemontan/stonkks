5030700f1d6d484e574000773885654378035679
Baptistemontan <baptiste.demontangon@gmail.com>
2022-12-30 17:50:02 +0100

    made default route work

--- test-server/src/main.rs
+++ test-server/src/main.rs
@@ -1,40 +1,92 @@
+use std::{ops::Deref, sync::Arc};
+
 use next_rs::prelude::*;
 use rocket::{
     fs::{relative, FileServer},
+    Route as RocketRoute,
     get, launch,
-    response::content::RawHtml,
-    routes, State,
+    response::{content::RawHtml, Responder},
+    routes, State, outcome::Outcome, route::Handler, Data, Response, http::{Status, ContentType, Method},
 };
 use test_client::get_app;
 
-use rocket::request::{FromRequest, Outcome, Request};
+use rocket::request::Request;
 
 struct Uri<'a>(pub UrlInfos<'a>);
 
-#[async_trait::async_trait]
-impl<'r> FromRequest<'r> for Uri<'r> {
-    type Error = String;
+impl<'a> Deref for Uri<'a> {
+    type Target = UrlInfos<'a>;
 
-    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let url = request.uri().path().as_str();
-        let url_infos = UrlInfos::parse_from_url(url);
-        Outcome::Success(Self(url_infos))
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
-#[get("/<_..>", rank = 11)]
-async fn hello(uri: Uri<'_>, app: &State<Server>) -> RawHtml<String> {
-    let Uri(url_infos) = uri;
-    println!("{}", url_infos.url());
-    let html = app.render_to_string(&url_infos).await;
-    RawHtml(html)
+impl<'a> Uri<'a> {
+    pub fn from_request(request: &'a Request<'_>) -> Self {
+        let url = request.uri().path().as_str();
+        let url_infos = UrlInfos::parse_from_url(url);
+        Self(url_infos)
+    }
 }
+
+#[derive(Clone)]
+struct MyServer(Arc<Server>);
+
+#[async_trait::async_trait]
+impl Handler for MyServer {
+    async fn handle<'r>(&self, request: &'r Request<'_>, data: Data<'r>) -> Outcome<Response<'r>, Status, Data<'r>>{
+        let url = Uri::from_request(request);
+        let result = self.0.try_render_to_string(&url).await;
+        match result {
+            Some(html) => {
+                let response = (ContentType::HTML, html).respond_to(request);
+                match response {
+                    Ok(rep) => Outcome::Success(rep),
+                    Err(status) => Outcome::Failure(status)
+                }
+            },
+            None => Outcome::Forward(data)
+        }
+    }
+}
+
+impl Into<Vec<RocketRoute>> for MyServer {
+    fn into(self) -> Vec<RocketRoute> {
+        vec![RocketRoute::new(Method::Get, "/<_..>", self)]
+    }
+}
+
+#[derive(Clone)]
+struct NotFound(Arc<Server>);
+
+#[async_trait::async_trait]
+impl Handler for NotFound {
+    async fn handle<'r>(&self, request: &'r Request<'_>, _data: Data<'r>) -> Outcome<Response<'r>, Status, Data<'r>>{
+        let html = self.0.render_not_found();
+        let response = (Status::NotFound, (ContentType::HTML, html)).respond_to(request);
+        match response {
+            Ok(rep) => Outcome::Success(rep),
+            Err(status) => Outcome::Failure(status)
+        }
+    }
+}
+
+impl Into<Vec<RocketRoute>> for NotFound {
+    fn into(self) -> Vec<RocketRoute> {
+        vec![RocketRoute::ranked(isize::MAX - 1, Method::Get, "/<_..>", self)]
+    }
+}
+
 
 #[launch]
 fn rocket() -> _ {
-    let server = get_app().into_server();
+    let app = get_app().into_server();
+    let app = Arc::new(app);
+    let server = MyServer(Arc::clone(&app));
+    let not_found = NotFound(app);
     rocket::build()
-        .manage(server)
-        .mount("/public", FileServer::from(relative!("static")))
-        .mount("/", routes![hello])
+        .mount("/public", FileServer::from(relative!("static")).rank(2))
+        .mount("/", server)
+        .mount("/", not_found)
 }
